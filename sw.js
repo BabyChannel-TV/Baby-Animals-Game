e0e5ea8e0dc507da6360d5134536eab018e1ce47
BabyChannel-TV <quietbabytv@gmail.com>
2026-06-10 04:12:48 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,17 @@
-const CACHE = 'baby-balloons-v2';
+const CACHE = 'baby-balloons-v1';
 const ASSETS = [
-  './index.html',
+  './baby_balloons_FINAL10.html',
   './manifest.json',
   './icon-192.png',
   './icon-512.png'
 ];
+
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
   );
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -17,6 +19,7 @@ self.addEventListener('activate', e => {
     ).then(() => self.clients.claim())
   );
 });
+
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
